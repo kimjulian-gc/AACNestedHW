4fba89de8344191b92c78f5a42db472be2c450d1
kerabite <10791585+kerabite@users.noreply.github.com>
2023-10-12 01:06:23 -0500

    Fix AACCategory formatting

--- src/AACCategory.java
+++ src/AACCategory.java
@@ -2,6 +2,9 @@ import structures.AssociativeArray;
 import structures.KeyNotFoundException;
 
 public class AACCategory {
+  /**
+   * Fields
+   */
   String categoryName;
   AssociativeArray<String, String> textImageMap;
 
@@ -19,17 +22,21 @@ public class AACCategory {
   public void addItem(String imageLoc, String text) {
     textImageMap.set(imageLoc, text);
   }
+
   public String getCategory() {
     return categoryName;
   }
+
   public String[] getImages() {
     return textImageMap.getKeys();
   }
+
   public String getText(String imageLoc) throws KeyNotFoundException {
     // if this throws an exception, we have screwed up somewhere.
     // probably forgot to add an image.
     return textImageMap.get(imageLoc);
   }
+  
   public boolean hasImage(String imageLoc) {
     try {
       textImageMap.get(imageLoc);
