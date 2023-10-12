062ee8c93f769f6d7affb260916696d2f0ced8de
kerabite <10791585+kerabite@users.noreply.github.com>
2023-10-11 23:20:43 -0500

    Add stubs for AACCategory and AACMappings

---
+++ src/AACCategory.java
@@ -0,0 +1,31 @@
+public class AACCategory {
+  /**
+   * Constructor
+   */
+  public AACCategory(String name) {
+    // stub
+  }
+
+  /**
+   * Methods
+   */
+  public void addItem(String imageLoc, String text) {
+    // stub
+  }
+  public String getCategory() {
+    return "";
+    // stub
+  }
+  public String[] getImages() {
+    return new String[] {""};
+    // stub
+  }
+  public String getText(String imageLoc) {
+    return "";
+    // stub
+  }
+  public boolean hasImage(String imageLoc) {
+    return false;
+    // stub
+  }
+}
---
+++ src/AACMappings.java
@@ -0,0 +1,36 @@
+public class AACMappings {/**
+   * Constructor
+   */
+  public AACMappings(String filename) {
+
+  }
+
+  /**
+   * Methods
+   */
+  public void add(String imageLoc, String text) {
+    // stub
+  }
+  public String getCurrentCategory() {
+    return "";
+    // stub
+  }
+  public String[] getImageLocs() {
+    return new String[] {""};
+    // stub
+  }
+  public String getText(String imageLoc) {
+    return "";
+    // stub
+  }
+  public boolean isCategory(String imageLoc) {
+    return false;
+    // stub
+  }
+  public void reset() {
+    // stub
+  }
+  public void writeToFile(String filename) {
+    // stub
+  }
+}
